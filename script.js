f7aa558baa888ef6470121e3c80671ed02193773
Karolis00 <alakvoscius.karolis@gmail.com>
2022-11-15 11:16:26 +0200

    Gets player input, alerts result and current score. Option to play again after winning/losing 5 times

--- script.js
+++ script.js
@@ -9,11 +9,10 @@ function getComputerChoice()
 
 function indexPlayerChoice(x)
 {
-    let choice;
+    let choice = -1;
     if(x.toUpperCase() === "ROCK") choice = 0;
     else if (x.toUpperCase() === "PAPER") choice = 1;
     else if (x.toUpperCase() === "SCISSORS") choice = 2;
-    else choice = -1; // bad input
     return choice;
 }
 
@@ -37,20 +36,40 @@ function toText(x)
 
 function getPlayerChoice()
 {
-    
+    return prompt("Choose rock/paper/scissors");
 }
 
-function playGame(playerChoice)
+function playGame()
 {
     let playerPoints = 0;
     let computerPoints = 0;
     let answer;
+    
     while( playerPoints < 5 && computerPoints < 5 )
     {
-        answer = playRound(playerChoice, getComputerChoice());
-        if(answer.charAt(0) === "W") playerPoints++;
-        else if(answer.charAt(0) === "L") computerPoints++;
-        alert(answer);
-        playerChoice = getPlayerChoice();
+        playerChoice = indexPlayerChoice(getPlayerChoice());
+
+        if(playerChoice === -1) // check if user input was rock/paper/scissors
+        {
+            alert("NOT A VALID CHOICE");
+        }
+        else // Only play game if proper user input
+        {
+            answer = playRound(playerChoice, getComputerChoice());
+
+            if(answer.charAt(0) === "W") playerPoints++;
+            else if(answer.charAt(0) === "L") computerPoints++;
+
+            alert(answer);
+            alert("Score is " + playerPoints + " | " + computerPoints);
+        }
     }
+
+    if(computerPoints === 5) alert("COMPUTER WINS!");
+    else alert("YOU WIN, PLAYER!");
+
+    if(prompt("Play again?", "Yes").toUpperCase() === "YES")
+    playGame();
 }
+
+playGame();
