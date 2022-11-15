9d88180d93d6bd02fbd68061aeb2dc1162da12d3
Karolis00 <alakvoscius.karolis@gmail.com>
2022-11-15 10:07:41 +0200

    assign index to player choice

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function getComputerChoice()
 {
     return Math.floor(Math.random() * 3 );
 }
+
+function getPlayerChoice(x)
+{
+    let choice;
+    if(x.toUpperCase() === "ROCK") choice = 0;
+    else if (x.toUpperCase() === "PAPER") choice = 1;
+    else if (x.toUpperCase() === "SCISSORS") choice = 2;
+    else choice = -1; // bad input
+    return choice;
+}
+
